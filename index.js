502dcc8ed5df72125dc4900c80265cffde8db92c
Unique Tandukar <uniquetandukar143@gmail.com>
2023-08-01 05:27:05 -0700

    learned about variables

---
+++ index.js
@@ -0,0 +1,17 @@
+//variable(var,let,const)
+//ESS
+
+var ram="person";
+
+ram="shyam"; 
+
+console.log(ram);
+
+//ESG
+const person1="ram";
+console.log({person1});
+//let
+
+let person2="ram";
+person2="shyam";
+console.log({person2});
