870180f5bcbd51370fbc9b96b09aa8a391a9d81e
Unique Tandukar <uniquetandukar143@gmail.com>
2023-08-02 05:12:15 -0700

    lab 3

--- index.js
+++ index.js
@@ -1,83 +1,109 @@
-// // // //variable(var,let,const)
-// // // //ESS
+// // // // //variable(var,let,const)
+// // // // //ESS
 
-// // // var ram="person";
+// // // // var ram="person";
 
-// // // ram="shyam"; 
+// // // // ram="shyam"; 
 
-// // // console.log(ram);
+// // // // console.log(ram);
 
-// // // //ESG
-// // // const person1="ram";
-// // // console.log({person1});
-// // // //let
+// // // // //ESG
+// // // // const person1="ram";
+// // // // console.log({person1});
+// // // // //let
 
-// // // let person2="ram";
-// // // person2="shyam";
-// // // console.log({person2});
-// // //chapter 2 -->interactions
-// // alert("user and pasword is wrong");
+// // // // let person2="ram";
+// // // // person2="shyam";
+// // // // console.log({person2});
+// // // //chapter 2 -->interactions
+// // // alert("user and pasword is wrong");
 
-// // const name=prompt("enter your name");
-// // const pw=prompt("enter your password");
+// // // const name=prompt("enter your name");
+// // // const pw=prompt("enter your password");
 
-// // confirm("are you sure?");
+// // // confirm("are you sure?");
 
-// // const age=confirm("are you 18 yrs and above?");
-// // console.log({name});
+// // // const age=confirm("are you 18 yrs and above?");
+// // // console.log({name});
 
 
 
-// // if(name==="unique" && pw==="123")
-// // {
-// //     alert("welcome to our website");
+// // // if(name==="unique" && pw==="123")
+// // // {
+// // //     alert("welcome to our website");
+// // // }
+
+
+
+
+// // // const id=prompt("enter your id");
+// // // const pw=prompt("input password");
+
+// // // if(id==="0098" && pw==="nee098")
+// // // {
+// // //     alert("you are verified");
+// // // }
+// // // //output
+
+
+
+// // //change km into 
+
+// // //chapter 3
+// // const hour=prompt("enter hour");
+// // //let hour = 17;
+// // if (hour > 10 && hour < 15) { 
+// // alert("JS Class is in session");
+// //  }
+
+// //  const isValid = true;//false rakhe nih huncha
+// //  if(!valid)
+// //  {
+// //     alert("it's not valid");
+// //  }
+
+
+
+//  //statements
+
+// //const a =prompt("enter a");
+// //let a=0;
+// // if(a>0){
+// //     alert("a is positive");
+// // }
+// // else{
+// //     alert("a is negative");
 // // }
 
 
+// //ternary operator7
 
-
-// // const id=prompt("enter your id");
-// // const pw=prompt("input password");
-
-// // if(id==="0098" && pw==="nee098")
-// // {
-// //     alert("you are verified");
-// // }
-// // //output
+// //a> 0 ? alert("a is positive") : ("a is negative");
 
 
 
-// //change km into 
-
-// //chapter 3
-// const hour=prompt("enter hour");
-// //let hour = 17;
-// if (hour > 10 && hour < 15) { 
-// alert("JS Class is in session");
-//  }
-
-//  const isValid = true;//false rakhe nih huncha
-//  if(!valid)
-//  {
-//     alert("it's not valid");
-//  }
-
-
-
- //statements
-
-const a =prompt("enter a");
-//let a=0;
-// if(a>0){
-//     alert("a is positive");
+// //for loop
+// for(a=0;a<10;a++)
+// {
+//     console.log(a);
+    
 // }
-// else{
-//     alert("a is negative");
+
+// while loop 
+
+// let i = 1;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
 // }
 
 
-//ternary operator7
+//do while
+let i=1;    
+do{    
+   console.log(i);    
+i++;    
+}while(i<=10);    
 
-//a> 0 ? alert("a is positive") : ("a is negative");
 
 
