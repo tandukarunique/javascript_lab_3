bfcf0386892d8800aa08baf948e4f3cee1b1ec70
Unique Tandukar <uniquetandukar143@gmail.com>
2023-08-06 04:54:32 -0700

    fundamental javascript

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-// // // // //variable(var,let,const)
-// // // // //ESS
+// // // // // //variable(var,let,const)
+// // // // // //ESS
 
-// // // // var ram="person";
+// // // // // var ram="person";
 
-// // // // ram="shyam"; 
+// // // // // ram="shyam"; 
 
-// // // // console.log(ram);
+// // // // // console.log(ram);
 
-// // // // //ESG
-// // // // const person1="ram";
-// // // // console.log({person1});
-// // // // //let
+// // // // // //ESG
+// // // // // const person1="ram";
+// // // // // console.log({person1});
+// // // // // //let
 
-// // // // let person2="ram";
-// // // // person2="shyam";
-// // // // console.log({person2});
-// // // //chapter 2 -->interactions
-// // // alert("user and pasword is wrong");
+// // // // // let person2="ram";
+// // // // // person2="shyam";
+// // // // // console.log({person2});
+// // // // //chapter 2 -->interactions
+// // // // alert("user and pasword is wrong");
 
-// // // const name=prompt("enter your name");
-// // // const pw=prompt("enter your password");
+// // const name=prompt("enter your name");
+// // const pw=prompt("enter your password");
 
-// // // confirm("are you sure?");
+// // // // confirm("are you sure?");
 
-// // // const age=confirm("are you 18 yrs and above?");
-// // // console.log({name});
-
-
-
-// // // if(name==="unique" && pw==="123")
-// // // {
-// // //     alert("welcome to our website");
-// // // }
+// // const age=confirm("are you 18 yrs and above?");
+// // console.log({name});
 
 
 
-
-// // // const id=prompt("enter your id");
-// // // const pw=prompt("input password");
-
-// // // if(id==="0098" && pw==="nee098")
-// // // {
-// // //     alert("you are verified");
-// // // }
-// // // //output
+// // if(name==="unique" && pw==="123")
+// // {
+// //     alert("welcome to our website");
+// // }
 
 
 
-// // //change km into 
 
-// // //chapter 3
-// // const hour=prompt("enter hour");
-// // //let hour = 17;
-// // if (hour > 10 && hour < 15) { 
-// // alert("JS Class is in session");
-// //  }
+// const id=prompt("enter your id");
+// const pw=prompt("input password");
 
-// //  const isValid = true;//false rakhe nih huncha
-// //  if(!valid)
-// //  {
-// //     alert("it's not valid");
-// //  }
+// if(id==="0098" && pw==="nee098")
+// {
+//     alert("you are verified");
+// }
+// //output
 
 
 
-//  //statements
+// // // //change km into 
 
-// //const a =prompt("enter a");
-// //let a=0;
-// // if(a>0){
+// // // //chapter 3
+// // // const hour=prompt("enter hour");
+// // // //let hour = 17;
+// // // if (hour > 10 && hour < 15) { 
+// // // alert("JS Class is in session");
+// // //  }
+
+// // //  const isValid = true;//false rakhe nih huncha
+// // //  if(!valid)
+// // //  {
+// // //     alert("it's not valid");
+// // //  }
+
+
+
+// //  //statements
+
+// // const a =prompt("enter a");
+
+// // if(a>5){
 // //     alert("a is positive");
 // // }
 // // else{
@@ -76,34 +76,77 @@
 // // }
 
 
-// //ternary operator7
+// // //ternary operator7
 
-// //a> 0 ? alert("a is positive") : ("a is negative");
+// // //a> 0 ? alert("a is positive") : ("a is negative");
 
 
 
-// //for loop
-// for(a=0;a<10;a++)
-// {
-//     console.log(a);
+// // //for loop
+// // for(a=0;a<10;a++)
+// // {
+// //     console.log(a);
     
-// }
+// // }
 
-// while loop 
+// // while loop 
 
-// let i = 1;
-// while (i < 10) {
-//   console.log(i);
-//   i++;
-// }
-
-
-//do while
-let i=1;    
-do{    
-   console.log(i);    
-i++;    
-}while(i<=10);    
+// // let i = 1;
+// // while (i < 10) {
+// //   console.log(i);
+// //   i++;
+// // }
 
 
+// //do while
+// // let i=1;    
+// // do{    
+// //    console.log(i);    
+// // i++;    
+// // }while(i<=10);    
 
+
+// //array
+// // const arr=["ram","shyam",46,{},true,[]];
+// // console.log(arr.length);
+// // console.log(arr[0]);
+// // console.log(arr.pop{});   //reamaining
+// // console.log(arr.push{});
+
+// // const persons = 
+// // [{name:"ram", age : 20},
+// // {name:"shyam", age : 5},
+// // {name:"hari", age : 10},
+// // ];
+
+// // const age = person.sort((a,b)=>{
+// // return a.age-b.age;
+// // });
+// // console.log(age); 
+
+// // length,sort,push,PopStateEvent
+// //map,find,filter,reduce//immutable js
+// //cancat,join,slice,splice,
+
+// const personfunction=( person)=>
+// {
+//     //starting
+//     return person.name;
+// };
+// const newperson2= person.map(person.function);  //starting
+
+// const newpersons = persons.map((person))=> {
+//     //2nd step
+//     return person.name;
+// });
+
+// const newperson = persons.map((person)=> person.name);  //final step
+
+// console.log({ persons,newperson, newpersons,newperson2});
+
+
+
+
+
+
+//
